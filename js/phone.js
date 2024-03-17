ce0fed2d137c60d9198ba515af497e0ce0b8035d
ShakirEvan530 <ausshakirahmed@gmail.com>
2024-03-17 21:57:26 +0600

    show all button part done

--- js/phone.js
+++ js/phone.js
@@ -1,13 +1,13 @@
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText,isShowAll) => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${searchText}`);
   const data = await res.json();
   const phones = data.data;
 //   console.log(phones);
-  displayPhones(phones);
+  displayPhones(phones,isShowAll);
 };
 
-const displayPhones = phones => {
+const displayPhones = (phones,isShowAll) => {
   console.log(phones);
   const phoneContainer = document.getElementById('phone-container');
 
@@ -15,14 +15,17 @@ phoneContainer.textContent = '';
 
 // display show all button if there are more than 12 phones
 const showAllContainer = document.getElementById('show-all-container')
-if(phones.length>12){
+if(phones.length > 12 && !isShowAll){
     showAllContainer.classList.remove('hidden');
 }
 else{
     showAllContainer.classList.add('hidden');
 }
-// display only first 12 phones
-phones = phones.slice(0,12);
+console.log('is show all',isShowAll);
+// display only first 12 phones if not show All
+if(!isShowAll){
+  phones = phones.slice(0,12);
+}
 
   phones.forEach(phone =>{
     // console.log(phone);
@@ -37,7 +40,7 @@ phones = phones.slice(0,12);
           <h2 class="card-title">${phone.phone_name}</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
           <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
+            <button onclick="" class="btn btn-primary">Show Details</button>
           </div>
         </div>
         `;
@@ -49,21 +52,21 @@ phones = phones.slice(0,12);
 
 
 // handle search button 
-const handleSearch =() =>{
+const handleSearch =(isShowAll) =>{
   toggleLoadingSpinner(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     console.log(searchText);
-    loadPhone(searchText);
+    loadPhone(searchText,isShowAll);
 }
 
 // handle search recap
-const handleSearch2 =() =>{
-  toggleLoadingSpinner(true);
-    const searchField = document.getElementById('search-field2')
-    const searchText = searchField.value;
-    loadPhone(searchText);
-}
+// const handleSearch2 =() =>{
+//   toggleLoadingSpinner(true);
+//     const searchField = document.getElementById('search-field2')
+//     const searchText = searchField.value;
+//     loadPhone(searchText);
+// }
 
 const toggleLoadingSpinner = (isLoading) =>{
   const loadingSpinner = document.getElementById('loading-spinner');
@@ -78,6 +81,6 @@ const toggleLoadingSpinner = (isLoading) =>{
 // handle show all 
 
 const handleShowAll = () =>{
-  handleSearch();
+  handleSearch(true);
 }
 // loadPhone();
